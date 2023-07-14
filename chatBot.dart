0975101c962e7a5503cd561e4c8b62300ac677f1
fab-ani <111348845+fab-ani@users.noreply.github.com>
2023-07-14 05:21:00 -0700

    profilimage.dart

--- chatBot.dart
+++ chatBot.dart
@@ -23,23 +23,137 @@ class ProfileImage extends StatefulWidget {
 }
 
 class _ProfileImageState extends State<ProfileImage> {
-  List<File>? _image = [];
-  late cloud_store.CollectionReference imgref;
+  List<File>? _image;
   bool _uploading = false;
   String _uploadStatus = "";
 
-  final TextEditingController productName = TextEditingController();
-  final TextEditingController productPrice = TextEditingController();
-  final TextEditingController productDescription = TextEditingController();
-  final TextEditingController productStock = TextEditingController();
+  final productName = TextEditingController();
+  final productPrice = TextEditingController();
+  final productDescription = TextEditingController();
+  final productStock = TextEditingController();
 
-  @override
-  void dispose() {
-    productName.dispose();
-    productPrice.dispose();
-    productDescription.dispose();
-    productStock.dispose();
-    super.dispose();
+  Future<List<String>> uploadFiles(List<File> files) async {
+    List<String> downloadUrls = [];
+    final storageRef = FirebaseStorage.instance.ref();
+
+    final uploadTasks = files.map((file) {
+      final fileName = DateTime.now().microsecondsSinceEpoch.toString();
+      final fileRef = storageRef.child('images/$fileName.png');
+      return fileRef.putFile(file);
+    }).toList();
+    final snapShots = await Future.wait(uploadTasks);
+    for (final snapshot in snapShots) {
+      final downloadUrl = await snapshot.ref.getDownloadURL();
+      downloadUrls.add(downloadUrl);
+      print(downloadUrl);
+    }
+    return downloadUrls;
+  }
+
+  Future<void> _uploadImage() async {
+    if (_image == null) return;
+    final user = Provider.of<Users>(context, listen: false);
+
+    setState(() {
+      _uploading = true;
+      _uploadStatus = "Uploading image...";
+    });
+    String? fileURL;
+    List<String>? downloadURL;
+    if (_image!.length == 1) {
+      final imageName = '${DateTime.now().millisecondsSinceEpoch}.png';
+      final firebaseStorageRef =
+          FirebaseStorage.instance.ref().child('images/$imageName');
+      final uploadTask = firebaseStorageRef.putFile(_image![0]);
+      try {
+        final taskSnapshot = await uploadTask;
+        fileURL = await taskSnapshot.ref.getDownloadURL();
+        setState(() {
+          _uploadStatus = 'Succeded';
+        });
+      } catch (e) {
+        setState(() {
+          _uploadStatus = 'failed $e';
+        });
+      }
+    }
+    if (_image!.isNotEmpty) {
+      try {
+        downloadURL = await uploadFiles(_image!);
+        setState(() {
+          _uploadStatus = 'Images uploaded successfully';
+        });
+      } catch (e) {
+        setState(() {
+          _uploadStatus = 'Failed to upload images: $e';
+        });
+      }
+    }
+
+    // ignore: use_build_context_synchronously
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Add product Details"),
+            content: Column(
+              children: [
+                TextFormField(
+                  controller: productName,
+                  decoration: const InputDecoration(labelText: "Product Name"),
+                ),
+                TextFormField(
+                  controller: productPrice,
+                  decoration: const InputDecoration(labelText: "Product Price"),
+                ),
+                TextFormField(
+                  controller: productDescription,
+                  decoration:
+                      const InputDecoration(labelText: "Product Description"),
+                ),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  final name = productName.text;
+                  final price = int.parse(productPrice.text);
+                  final details = productDescription.text;
+                  final stock = int.parse(productStock.text);
+                  final List<String> imageUrls = [];
+                  if (fileURL != null) {
+                    imageUrls.add(fileURL);
+                    print('my images urls here $imageUrls');
+                  }
+                  if (downloadURL != null) {
+                    imageUrls.addAll(downloadURL);
+                    print('my images urls here $imageUrls');
+                  }
+                  try {
+                    await DatabaseServices(uid: user.uid!).updateUserData(
+                      name,
+                      price,
+                      details,
+                      imageUrls,
+                      stock,
+                    );
+                    setState(() {
+                      _uploadStatus = 'Data updated successfully!';
+                    });
+                  } catch (e) {
+                    _uploadStatus = 'failed to update: $e';
+                  }
+                },
+                child: const Text("upload"),
+              ),
+            ],
+          );
+        });
+
+    setState(() {
+      _uploading = false;
+    });
   }
 
   @override
@@ -54,11 +168,11 @@ class _ProfileImageState extends State<ProfileImage> {
           icon: const Icon(Icons.arrow_back_ios_new),
         ),
         actions: [
-          ElevatedButton(
+          ElevatedButton.icon(
             onPressed: _uploading ? null : _uploadImage,
-            child: _uploading
-                ? const CircularProgressIndicator()
-                : const Icon(Icons.arrow_upward),
+            icon: const Icon(Icons.upload),
+            label:
+                _uploading ? Text(_uploadStatus) : const Text('Upload image'),
           ),
         ],
       ),
@@ -135,126 +249,5 @@ class _ProfileImageState extends State<ProfileImage> {
         ],
       ),
     );
-  }
-
-  Future<List<String>> uploadFiles(List<File> files) async {
-    List<String> downloadUrls = [];
-    final storageRef = FirebaseStorage.instance.ref();
-
-    final uploadTasks = files.map((file) {
-      final fileName = DateTime.now().microsecondsSinceEpoch.toString();
-      final fileRef = storageRef.child('images/$fileName');
-      return fileRef.putFile(file);
-    }).toList();
-    final snapShots = await Future.wait(uploadTasks);
-    for (final snapshot in snapShots) {
-      final downloadUrl = await snapshot.ref.getDownloadURL();
-      downloadUrls.add(downloadUrl);
-      print(downloadUrl);
-    }
-    return downloadUrls;
-  }
-
-  Future<void> _uploadImage() async {
-    if (_image == null) return;
-    final user = Provider.of<Users>(context, listen: false);
-    print('uploading ');
-
-    setState(() {
-      _uploading = true;
-      _uploadStatus = "Uploading image...";
-    });
-    String? fileURL;
-    List<String>? downloadURL;
-    if (_image!.length == 1) {
-      final imageName = '${DateTime.now().millisecondsSinceEpoch}.png';
-      final firebaseStorageRef =
-          FirebaseStorage.instance.ref().child('images/$imageName');
-      final uploadTask = firebaseStorageRef.putFile(_image![0]);
-      final taskSnapshot = await uploadTask;
-      fileURL = await taskSnapshot.ref.getDownloadURL();
-    }
-    if (_image!.isNotEmpty) {
-      try {
-        downloadURL = await uploadFiles(_image!);
-        setState(() {
-          _uploadStatus = 'Images uploaded successfully';
-        });
-      } catch (e) {
-        setState(() {
-          _uploadStatus = 'Failed to upload images: $e';
-        });
-      }
-    }
-
-    // ignore: use_build_context_synchronously
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("Add product Details"),
-            content: Column(
-              children: [
-                TextFormField(
-                  controller: productName,
-                  decoration: const InputDecoration(labelText: "Product Name"),
-                ),
-                TextFormField(
-                  controller: productPrice,
-                  decoration: const InputDecoration(labelText: "Product Price"),
-                ),
-                TextFormField(
-                  controller: productDescription,
-                  decoration:
-                      const InputDecoration(labelText: "Product Description"),
-                ),
-              ],
-            ),
-            actions: [
-              ElevatedButton(
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  final name = productName.text;
-                  final price = int.parse(productPrice.text);
-                  final details = productDescription.text;
-                  final stock = int.parse(productStock.text);
-                  final List<String> imageUrls = [];
-                  if (fileURL != null) {
-                    imageUrls.add(fileURL);
-                    print('my images urls here $imageUrls');
-                  } else {
-                    print('my fileurl is null');
-                  }
-                  if (downloadURL != null) {
-                    imageUrls.addAll(downloadURL);
-                    print('my images urls here $imageUrls');
-                  } else {
-                    print('downloadurl is empty');
-                  }
-                  try {
-                    await DatabaseServices(uid: user.uid!).updateUserData(
-                      name,
-                      price,
-                      details,
-                      imageUrls,
-                      stock,
-                    );
-                    setState(() {
-                      _uploadStatus = 'Data updated successfully!';
-                    });
-                  } catch (e) {
-                    print('error here');
-                  }
-                },
-                child: const Text("upload"),
-              ),
-            ],
-          );
-        });
-
-    setState(() {
-      print('end');
-      _uploading = false;
-    });
   }
 }
